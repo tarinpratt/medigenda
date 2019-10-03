dd3df3afe55e76ad4e7f9bf841b9f6386f36a6ce
Tarin Pratt <tarinpratt@gmail.com>
2019-10-03 16:50:10 -0700

    p element issue

--- src/homePage/homePage.js
+++ src/homePage/homePage.js
@@ -13,7 +13,7 @@ class HomePage extends Component {
             TokenService.hasAuthToken()?
             null
             : <p>With MediGenda, you can give your memory a rest by storing details regarding upcoming doctors appointments, past doctors visits, billing, and medication intake with our easy to use record keeper.
-              <span><p>Try out our demo in the login page.</p></span>
+              <span><div className="demo">Try out our demo in the login page.</div></span>
               </p>   
           } 
       <section className="pillImage"></section>
