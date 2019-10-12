b998c285b7f2776f9186fc9034e279f0f56e0137
Tarin Pratt <tarinpratt@gmail.com>
2019-10-11 17:17:01 -0700

    nav

--- src/Nav/Nav.test.js
+++ src/Nav/Nav.test.js
@@ -1,5 +1,5 @@
-import React from './node_modules/react';
-import ReactDOM from './node_modules/react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from './node_modules/react-router-dom'
 import Nav from './Nav';
 
