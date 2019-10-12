5ed94841d2d1ba816a1adc8a3ded979dac89e299
Tarin Pratt <tarinpratt@gmail.com>
2019-10-11 17:17:45 -0700

    nav

--- src/Nav/Nav.test.js
+++ src/Nav/Nav.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from './node_modules/react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import Nav from './Nav';
 
 it('renders without crashing', () => {
